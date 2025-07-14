d25ea818d4cc30b5e2f440db642c94f594e104cd
PRITAM GHOSH <babupritam9933@gmail.com>
2025-07-15 05:05:08 +0530

    15/07/25 Pritam
    navber update

--- EMWeb/src/components/Navigation/Navigation.jsx
+++ EMWeb/src/components/Navigation/Navigation.jsx
@@ -1,22 +1,22 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./Navigation.css"
-const Navigation = ()=>{
+const Navigation = () => {
   return (
     <nav>
-        <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/find-events">Find Events</Link>
-            </li>
-            <li>
-              <Link to="/create-event">Create Event</Link>
-            </li>
-            <li>
-              <Link to="/Login">Login</Link>
-            </li>
-        </ul>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/find-events">Find Events</Link>
+        </li>
+        <li>
+          <Link to="/create-event">Create Event</Link>
+        </li>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
